19d2d9703c06a3569aad2cb6266a26798c441a8c
Pavan Arani <pavanarani00@gmail.com>
2025-11-30 03:17:22 -0600

    type ignore

--- libms.py
+++ libms.py
@@ -4,7 +4,7 @@ import os
 # Add the app directory to the path so imports work correctly
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'app'))
 
-from ui.gui import main
+from ui.gui import main # type: ignore
 
 if __name__ == "__main__":
     main()
